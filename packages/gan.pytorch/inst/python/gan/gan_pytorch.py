b80e335f362f25de98ae9a3dd1dd2029f5b2ebf3
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-08-30 15:25:07 -0500

    original GAN Python file

--- packages/gan.pytorch/inst/python/gan/gan_pytorch.py
+++ packages/gan.pytorch/inst/python/gan/gan_pytorch.py
@@ -3,57 +3,42 @@
 # Generative Adversarial Networks (GAN) example in PyTorch. Tested with PyTorch 0.4.1, Python 3.6.7 (Nov 2018)
 # See related blog post at https://medium.com/@devnag/generative-adversarial-networks-gans-in-50-lines-of-code-pytorch-e81b79659e3f#.sch4xgsa9
 
-from py_functions import fivenum
 import numpy as np
-import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 
-
-t = time.time()
-
 matplotlib_is_available = True
 try:
-    from matplotlib import pyplot as plt
+  from matplotlib import pyplot as plt
 except ImportError:
-    print("Will skip plotting; matplotlib is not available.")
-    matplotlib_is_available = False
-
-# # seeds
-# seed = 5960
-# torch.manual_seed(seed)
-# np.random.seed(seed=seed)
+  print("Will skip plotting; matplotlib is not available.")
+  matplotlib_is_available = False
 
 # Data params
 data_mean = 4
 data_stddev = 1.25
 
 # ### Uncomment only one of these to define what data is actually sent to the Discriminator
-# (name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
-# (name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
-# (name, preprocess, d_input_func) = ("Data and diffs", lambda data: decorate_with_diffs(data, 1.0), lambda x: x * 2)
+#(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
+#(name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
+#(name, preprocess, d_input_func) = ("Data and diffs", lambda data: decorate_with_diffs(data, 1.0), lambda x: x * 2)
 (name, preprocess, d_input_func) = ("Only 4 moments", lambda data: get_moments(data), lambda x: 4)
 
 print("Using data [%s]" % (name))
 
 # ##### DATA: Target data and generator input data
 
-
 def get_distribution_sampler(mu, sigma):
-    # Bell curve
     return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
 
-
 def get_generator_input_sampler():
-    return lambda m, n: torch.rand(m, n)   # Uniform-dist data into generator, _NOT_ Gaussian
-
+    return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
 
 # ##### MODELS: Generator model and discriminator model
+
 class Generator(nn.Module):
-    # Two hidden layers
-    # Three linear maps
     def __init__(self, input_size, hidden_size, output_size, f):
         super(Generator, self).__init__()
         self.map1 = nn.Linear(input_size, hidden_size)
@@ -69,9 +54,7 @@ class Generator(nn.Module):
         x = self.map3(x)
         return x
 
-
 class Discriminator(nn.Module):
-    # also two hidden layer and three linear maps
     def __init__(self, input_size, hidden_size, output_size, f):
         super(Discriminator, self).__init__()
         self.map1 = nn.Linear(input_size, hidden_size)
@@ -84,14 +67,11 @@ class Discriminator(nn.Module):
         x = self.f(self.map2(x))
         return self.f(self.map3(x))
 
-
 def extract(v):
     return v.data.storage().tolist()
 
-
 def stats(d):
     return [np.mean(d), np.std(d)]
-
 
 def get_moments(d):
     # Return the first 4 moments of the data provided
@@ -105,16 +85,14 @@ def get_moments(d):
     final = torch.cat((mean.reshape(1,), std.reshape(1,), skews.reshape(1,), kurtoses.reshape(1,)))
     return final
 
-
-# def decorate_with_diffs(data, exponent, remove_raw_data=False):
-#     mean = torch.mean(data.data, 1, keepdim=True)
-#     mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
-#     diffs = torch.pow(data - Variable(mean_broadcast), exponent)
-#     if remove_raw_data:
-#         return torch.cat([diffs], 1)
-#     else:
-#         return torch.cat([data, diffs], 1)
-
+def decorate_with_diffs(data, exponent, remove_raw_data=False):
+    mean = torch.mean(data.data, 1, keepdim=True)
+    mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
+    diffs = torch.pow(data - Variable(mean_broadcast), exponent)
+    if remove_raw_data:
+        return torch.cat([diffs], 1)
+    else:
+        return torch.cat([data, diffs], 1)
 
 def train():
     # Model parameters
@@ -130,15 +108,14 @@ def train():
     g_learning_rate = 1e-3
     sgd_momentum = 0.9
 
-    num_epochs = 500
+    num_epochs = 5000
     print_interval = 100
-    d_steps = 10
-    g_steps = 10
+    d_steps = 20
+    g_steps = 20
 
     dfe, dre, ge = 0, 0, 0
     d_real_data, d_fake_data, g_fake_data = None, None, None
 
-    # Activation functions
     discriminator_activation_function = torch.sigmoid
     generator_activation_function = torch.tanh
 
@@ -156,20 +133,18 @@ def train():
     d_optimizer = optim.SGD(D.parameters(), lr=d_learning_rate, momentum=sgd_momentum)
     g_optimizer = optim.SGD(G.parameters(), lr=g_learning_rate, momentum=sgd_momentum)
 
-    # Now, training loop alternates between Generator and Discriminator modes
-
     for epoch in range(num_epochs):
         for d_index in range(d_steps):
             # 1. Train D on real+fake
             D.zero_grad()
 
-            #  1A: Train D on real data
+            #  1A: Train D on real
             d_real_data = Variable(d_sampler(d_input_size))
             d_real_decision = D(preprocess(d_real_data))
             d_real_error = criterion(d_real_decision, Variable(torch.ones([1,1])))  # ones = true
-            d_real_error.backward()  # compute/store gradients, but don't change params
+            d_real_error.backward() # compute/store gradients, but don't change params
 
-            #  1B: Train D on fake data
+            #  1B: Train D on fake
             d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
             d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
             d_fake_decision = D(preprocess(d_fake_data.t()))
@@ -193,47 +168,19 @@ def train():
             ge = extract(g_error)[0]
 
         if epoch % print_interval == 0:
-            print("\t Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
+            print("Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
                   (epoch, dre, dfe, ge, stats(extract(d_real_data)), stats(extract(d_fake_data))))
 
     if matplotlib_is_available:
         print("Plotting the generated distribution...")
         values = extract(g_fake_data)
-        # print(" Values: %s" % (str(values)))
-        print(" fivenum: %s" % (str(fivenum(values))))
-        plt.hist(values, bins=50, alpha=0.5)
+        print(" Values: %s" % (str(values)))
+        plt.hist(values, bins=50)
         plt.xlabel('Value')
         plt.ylabel('Count')
         plt.title('Histogram of Generated Distribution')
         plt.grid(True)
-
-        # plt.show()
-        # plt.savefig("fig.pdf")
-        # time.sleep(0.1)
-        # plt.savefig("fig.pdf")
-        # plt.clf()
-        plt.draw()
         plt.show()
-        # plt.show(block=False)
-        plt.pause(0.1)
-
-    return values
 
 
-# for i in range(10):
-#     print("Run: ", i)
-#     train()
-#     #plt.draw()
-#     #plt.pause(0.1)
-
-
-# plt.show()
-
-
-ret_values = train()
-elapsed = time.time() - t
-print(elapsed)
-print("DONE!")
-
-
-print(fivenum(ret_values))
+train()
